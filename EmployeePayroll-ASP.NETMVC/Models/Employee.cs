adf030031d28caa311aec996d5698a6ed581c49e
Chetan <Chetan@DESKTOP-9SU09BS>
2020-12-20 20:42:44 +0530

    [Chetan]Add: Edit existing employee details.

--- EmployeePayroll-ASP.NETMVC/Models/Employee.cs
+++ EmployeePayroll-ASP.NETMVC/Models/Employee.cs
@@ -16,26 +16,65 @@ namespace EmployeePayroll_ASP.NETMVC.Models
         /// The emp identifier.
         /// </value>
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]        
         public int EmpId { get; set; }
-
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
-
+        /// <summary>
+        /// Gets or sets the gender.
+        /// </summary>
+        /// <value>
+        /// The gender.
+        /// </value>
         public string Gender { get; set; }
-
+        /// <summary>
+        /// Gets or sets the department identifier.
+        /// </summary>
+        /// <value>
+        /// The department identifier.
+        /// </value>
         public int DepartmentId { get; set; }
-
+        /// <summary>
+        /// Gets or sets the department.
+        /// </summary>
+        /// <value>
+        /// The department.
+        /// </value>
         [ForeignKey("DepartmentId")]
-
         public Department Department { get; set; }
+        /// <summary>
+        /// Gets or sets the salary identifier.
+        /// </summary>
+        /// <value>
+        /// The salary identifier.
+        /// </value>
         public int SalaryId { get; set; }
-
-        [ForeignKey("SalaryId")]
+        /// <summary>
+        /// Gets or sets the salary.
+        /// </summary>
+        /// <value>
+        /// The salary.
+        /// </value>
+        [ForeignKey("SalaryId")]        
         public Salary Salary { get; set; }
-
+        /// <summary>
+        /// Gets or sets the start date.
+        /// </summary>
+        /// <value>
+        /// The start date.
+        /// </value>
         public DateTime StartDate { get; set; }
-
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
         public string Description { get; set; }
     }
 }
