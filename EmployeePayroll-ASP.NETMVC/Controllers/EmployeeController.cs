7eac9b637a54c20fa6a6299159092fe22be9a73a
Chetan <Chetan@DESKTOP-9SU09BS>
2020-12-20 19:39:14 +0530

    [Chetan]Add: Get all Employees

--- EmployeePayroll-ASP.NETMVC/Controllers/EmployeeController.cs
+++ EmployeePayroll-ASP.NETMVC/Controllers/EmployeeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EmployeePayroll_ASP.NETMVC.Models;
+using EmployeePayroll_ASP.NETMVC.Models.Common;
+using EmployeePayroll_ASP.NETMVC.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,17 +9,39 @@ using System.Web.Mvc;
 
 namespace EmployeePayroll_ASP.NETMVC.Controllers
 {
+    [Authorize]
     public class EmployeeController : Controller
     {
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+
         // GET: Employee
         public ActionResult Index()
+        {
+            List<EmployeeViewModel> list = employeeRepository.GetEmployees();
+            return View(list);
+        }
+
+        // GET: Employee
+        public ActionResult Register()
         {
             return View();
         }
 
-        public ActionResult Register()
+        [HttpPost]
+        public ActionResult RegisterEmployee(RegisterRequestModel employee)
         {
-            return View();
+            bool result = false;
+            if (ModelState.IsValid)
+            {
+                result = employeeRepository.RegisterEmployee(employee);
+            }
+            ModelState.Clear();
+
+            if (result == true)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(employee);
         }
     }
 }
--- EmployeePayroll-ASP.NETMVC/Repository/EmployeeRepository.cs
+++ EmployeePayroll-ASP.NETMVC/Repository/EmployeeRepository.cs
@@ -9,14 +9,14 @@ namespace EmployeePayroll_ASP.NETMVC.Repository
 {
     public class EmployeeRepository
     {
-        ApplicationDbContext dbContext = new ApplicationDbContext();
+        public ApplicationDbContext db = new ApplicationDbContext();
         public List<EmployeeViewModel> GetEmployees()
         {
             try
             {
-                List<EmployeeViewModel> list = (from e in dbContext.Employees
-                                                join d in dbContext.Departments on e.DepartmentId equals d.DeptId
-                                                join s in dbContext.Salaries on e.SalaryId equals s.SalaryId
+                List<EmployeeViewModel> list = (from e in db.Employees
+                                                join d in db.Departments on e.DepartmentId equals d.DeptId
+                                                join s in db.Salaries on e.SalaryId equals s.SalaryId
                                                 select new EmployeeViewModel
                                                 {
                                                     EmpId = e.EmpId,
@@ -42,10 +42,10 @@ namespace EmployeePayroll_ASP.NETMVC.Repository
 
             try
             {
-                Employee validEmployee = dbContext.Employees.Where(x => x.Name == employee.Name && x.Gender == employee.Gender).FirstOrDefault();
+                Employee validEmployee = db.Employees.Where(x => x.Name == employee.Name && x.Gender == employee.Gender).FirstOrDefault();
                 if (validEmployee == null)
                 {
-                    int departmentId = dbContext.Departments.Where(x => x.DeptName == employee.Department).Select(x => x.DeptId).FirstOrDefault();
+                    int departmentId = db.Departments.Where(x => x.DeptName == employee.Department).Select(x => x.DeptId).FirstOrDefault();
                     Employee newEmployee = new Employee()
                     {
                         Name = employee.Name,
@@ -55,9 +55,9 @@ namespace EmployeePayroll_ASP.NETMVC.Repository
                         StartDate = employee.StartDate,
                         Description = employee.Description
                     };
-                    Employee returnData = dbContext.Employees.Add(newEmployee);
+                    Employee returnData = db.Employees.Add(newEmployee);
                 }
-                int result = dbContext.SaveChanges();
+                int result = db.SaveChanges();
                 if (result > 0)
                 {
                     return true;
@@ -66,6 +66,20 @@ namespace EmployeePayroll_ASP.NETMVC.Repository
                 {
                     return false;
                 }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            try
+            {
+                Employee list = db.Employees.Where(x => x.EmpId == id).SingleOrDefault();
+
+                return list;
             }
             catch (Exception e)
             {
